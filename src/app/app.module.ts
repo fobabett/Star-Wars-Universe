428edc0a0f485b512251ab6bc3e684e1b12d4f39
Te Vallee <astronomemusic@gmail.com>
2017-11-03 09:56:42 -1000

    created list and item detail components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { ItemDetailComponent } from './item-detail/item-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    ItemDetailComponent
   ],
   imports: [
     BrowserModule
