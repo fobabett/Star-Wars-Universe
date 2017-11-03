31eb9f91772dcc7a50e22ebfa9e9db3998ff086b
Te Vallee <astronomemusic@gmail.com>
2017-11-03 11:45:25 -1000

    service gets films from SWAPI

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { SWAPIService } from '../swapi.service';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +10,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  data : Array<any>;
+  itemsPerPage : number = 10;
+
+  constructor(private service: SWAPIService) {
+    this.data = this.service.data;
+    if(this.data === undefined) {
+      this.getData();
+    }
+  }
 
   ngOnInit() {
+  }
+
+  getData() {
+    this.data = [];
+    this.getFilms();
+    this.getPeople();
+    this.getPlanets();
+    this.getSpecies();
+    this.getStarShips();
+    this.getVehicles();
+  }
+
+  getFilms() {
+    let films;
+    this.service.getFilms(1)
+      .subscribe(
+        res => {
+          if(res.next !== null) {
+            let count : number = res.count;
+            let numOfPages = Math.ceil((count - res.results.length)/this.itemsPerPage);
+            let pages = Array.from({ length: numOfPages }, (v, i) => i);
+            Observable.forkJoin(
+              pages.map(
+                i => this.service.getFilms(i)
+                  .map(res => res.json())
+              )
+            ).subscribe(data => {
+              this.data = this.data.concat(res.results.map((item) => {
+                item.category = 'films';
+                item.id = item.url.split(`${item.category}/`)[1];
+                return item;
+              }));
+            });
+          } else {
+            this.data = this.data.concat(res.results.map((item) => {
+              item.category = 'films';
+              item.id = item.url.split(`${item.category}/`)[1];
+              return item;
+            }));
+          }
+        }, err => console.error(err)
+      )
+  }
+
+  getPeople() {
+
+  }
+
+  getPlanets() {
+
+  }
+
+  getSpecies() {
+
+  }
+
+  getStarShips() {
+
+  }
+  getVehicles() {
+
   }
 
 }
--- src/app/swapi.service.ts
+++ src/app/swapi.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class SWAPIService {
 
-  constructor() { }
+  data : Array<any>;
+  private apiUrl : string = 'https://swapi.co/api/';
 
+  constructor(private http: Http) { }
+
+  getFilms(page) {
+    return this.http.get(`${this.apiUrl}films/?page=${page}`)
+      .map(res => res.json())
+      .catch(this.handleError);
+  }
+
+  handleError(error: Response) {
+    return Observable.throw(error);
+  }
 }
