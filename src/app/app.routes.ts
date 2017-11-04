f13758c7edbe0d41d207cd6f732dcd7ed87729c6
Te Vallee <astronomemusic@gmail.com>
2017-11-03 16:51:10 -1000

    basic styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,7 @@ import { ModuleWithProviders } from '@angular/core';
 import { ListComponent } from './list';
 
 const AppRoutes : Routes = [
-  { path: '', component: ListComponent },
-  // { path: ':category/:id', component: ItemDetailComponent }
+  { path: '', component: ListComponent }
 ];
 
 export const appRoutingProviders: any[] = [];
